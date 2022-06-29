869059011ad8a5156d315e3f5399a3346ddac60c
Matheus Curvelo <matheuscurvelo@gmail.com>
2022-06-29 20:56:00 -0300

    Criando textos e botões

--- App.js
+++ App.js
@@ -1,112 +1,120 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
+import { Component } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+      alcool: '',
+      gasolina: ''
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+      <ScrollView>
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+
+        <View style={styles.topArea}>
+          <Image style={styles.image} source={require('./src/img/logo.png')} />
+
+          <Text style={styles.mainText}>
+            Qual a melhor opção?
+          </Text>
         </View>
+
+        <View style={styles.bottomArea}>
+
+          <Text style={styles.text}> 
+            Álcool (preço por litro):
+          </Text>
+          <TextInput style={styles.input} />
+
+          <Text style={styles.text}> 
+            Gasolina (preço por litro)
+          </Text>
+          <TextInput style={styles.input} />
+
+          <TouchableOpacity style={styles.buttonArea}>
+            <Text style={styles.buttonText}>
+              Calcular
+            </Text>
+          </TouchableOpacity>
+
+        </View>
+
+
       </ScrollView>
-    </SafeAreaView>
-  );
-};
+      </View>
+
+
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    backgroundColor: '#333333',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+
+  topArea: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 50
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+
+  mainText: {
+    color: '#FFF',
+    fontSize: 28,
+    fontWeight: 'bold'
   },
-  highlight: {
-    fontWeight: '700',
+
+  image: {
+    marginVertical: 20
   },
+
+  bottomArea: {
+    paddingBottom: 20
+  },
+
+  text: {
+    color: '#FFF',
+    fontSize: 14,
+    fontWeight: 'bold'
+  },
+
+  input: {
+    padding: 8,
+    marginVertical: 10,
+    backgroundColor: '#FFF',
+    borderRadius: 5
+  },
+
+  buttonArea: {
+    alignItems: 'center',
+    backgroundColor: '#EF4130',
+    borderRadius: 5
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    padding: 10,
+  }
 });
 
 export default App;
